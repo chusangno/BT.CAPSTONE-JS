8fb7a0ee01de59747906cb8fe169dc1701500f52
taidangdev <taidangdev@gmail.com>
2025-10-04 12:15:28 +0700

    upodate

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 
+// Lấy tên repo của bạn (ví dụ: BT.CAPSTONE-JS)
+const baseName = '/BT.CAPSTONE-JS/'; 
+
 export default defineConfig({
-  // Base URL cần thiết khi deploy lên GitHub Pages
-  base: '/BT.CAPSTONE-JS/', 
-})
+  base: baseName, // Đặt Base URL 
+
+  // Quan trọng: Thêm cấu hình build cho ứng dụng đa trang (MPA)
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        // Thêm các trang khác của bạn vào đây:
+        admin: 'admin/admin.html',
+        customer: 'customer/customer.html',
+      },
+    },
+  },
+});
